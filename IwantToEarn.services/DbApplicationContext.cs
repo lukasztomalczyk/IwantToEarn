7245bb1652c3466c1a139d9a965284aeed429bf2
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-02-23 16:41:00 +0100

    start project

---
+++ IwantToEarn.services/DbApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace IwantToEarn.services
+{
+    public class DbApplicationContext : DbContext
+    {
+        public DbApplicationContext(DbContextOptions<DbApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JobModel> JobModels { get; set; }
+    }
+}
---
+++ IwantToEarn.services/JobModel.cs
@@ -0,0 +1,8 @@
+namespace IwantToEarn
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
