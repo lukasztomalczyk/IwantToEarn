22f5837e05c82b0e29afefc4895cf8a980a19689
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-02-23 17:41:05 +0100

    controller work

--- IwantToEarn.services/JobModel.cs
+++ IwantToEarn.services/JobModel.cs
@@ -3,6 +3,10 @@ namespace IwantToEarn
     public class JobModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Where { get; set; }
+        public string WholeOrHour { get; set; }
+        public string Cash { get; set; }
+        public string Description { get; set; }
     }
 }
